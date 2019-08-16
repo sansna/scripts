f74ba05f8d466c8a37cd0a1f176bfb66da263c57
sansna <1185280650@qq.com>
2019-08-16 22:35:25 +0800

    @Wentao[master]: fix lua scripts and add new file for check - sansna

--- check.lua
+++ check.lua
@@ -127,23 +127,4 @@ elseif dur3 >= max3 then
 	return 3
 end
 
---- inc
-dur1 = redis.call('INCR', KEYS[1])
-
-if dur1 == 1 then
-	redis.call('EXPIRE', KEYS[1], ARGV[1])
-end
-
-dur2 = redis.call('INCR', KEYS[2])
-
-if dur2 == 1 then
-	redis.call('EXPIRE', KEYS[2], ARGV[2])
-end
-
-dur3 = redis.call('INCR', KEYS[3])
-
-if dur3 == 1 then
-	redis.call('EXPIRE', KEYS[3], ARGV[3])
-end
-
 return 0
