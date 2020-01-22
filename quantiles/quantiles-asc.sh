a4234c10f6d918236d0cacd9f6fd457032486f98
sansna <1185280650@qq.com>
2020-01-22 16:14:40 +0800

    @Wentao[master]: update - sansna

---
+++ quantiles/quantiles-asc.sh
@@ -0,0 +1,2 @@
+echo "TYPE\tCOUNT\t95%\t90%\t80%"
+sort -k2,2 -sn|sort -k1,1 -ns | gawk -vFS=' ' -vRS='\n' 'BEGIN{delete kv;h=0;i=0;delete kvlen;}{if (h!=$1){kvlen[h]=i;h=$1;i=0;}else{i+=1;};kv[$1,i]=$2}END{kvlen[h]=i;for (i in kvlen){print i"\t"kvlen[i]"\t"kv[i,int(kvlen[i]*0.05)]"\t"kv[i,int(kvlen[i]*0.1)]"\t"kv[i,int(kvlen[i]*0.2)]"\t"};}'
